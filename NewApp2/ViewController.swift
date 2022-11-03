c12cb81c0af878de1500c984dbf8202ecea6bec3
NutsCraker <farizanov@gmail.com>
2022-11-03 15:29:38 +0300

    Initial Commit

---
+++ NewApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp2
+//
+//  Created by Alexander Farizanov on 03.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
